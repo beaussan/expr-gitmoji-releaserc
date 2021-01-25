448530c2ac300a5022d4eb9e6992abe7ae990d68
Nicolas Beaussart <nic.beaussart@gmail.com>
2021-01-25 08:36:29 +0100

    :wrench: set default branch to main

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,12 @@
 module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
   plugins: [
     [
       'semantic-release-gitmoji',
@@ -34,7 +42,6 @@ module.exports = {
       },
     ],
     '@semantic-release/github',
-
     [
       '@semantic-release/git',
       {
