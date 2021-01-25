af406c7467e2f3d8da9aa915d0a055183a7097b2
Nicolas Beaussart <nic.beaussart@gmail.com>
2021-01-25 08:28:31 +0100

    :wrench: add scemantic release

---
+++ .releaserc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    plugins: [
+        [
+            'semantic-release-gitmoji',
+            {
+              releaseRules: {
+                major: [':boom:'],
+                minor: [':sparkles:'],
+                patch: [
+                  ':bug:',
+                  ':ambulance:',
+                  ':lipstick:',
+                  ':lock:',
+                  ':zap:',
+                  ':apple:',
+                  ':penguin:',
+                  ':checkered_flag:',
+                  ':robot:',
+                  ':green_apple:',
+                  ':chart_with_upwards_trend:',
+                  ':globe_with_meridians:',
+                  ':alien:',
+                  ':wheelchair:',
+                  ':loud_sound:',
+                  ':mute:',
+                  ':mag:',
+                  ':children_crossing:',
+                  ':speech_balloon:',
+                  ':iphone:',
+                  ':pencil2:',
+                  ':bento:',
+                ],
+              },
+            },
+          ],
+        '@semantic-release/github',
+
+    [
+        '@semantic-release/git',
+        {
+          assets: ['package.json'],
+          message:
+            ':bookmark: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        },
+      ],
+    ]
+}
